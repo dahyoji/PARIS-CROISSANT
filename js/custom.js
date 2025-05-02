e3776c8a9ace186e9f4146dc17308109510d7bc4
dahyoji <gkdud8429@naver.com>
2025-05-02 14:35:49 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,68 @@
+$(function () {
+    const main_visual_slide = new Swiper('.main_visual_slide', {
+        loop: true,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        speed: 6000,
+
+    });
+
+    $('.main_visual .prev').on('click', function () {
+        main_visual_slide.slidePrev();
+    });
+
+    $('.main_visual .next').on('click', function () {
+        main_visual_slide.slideNext();
+    });
+
+    const MMS = new Swiper('.main_menu_slide', {
+        loop: true,
+        slidesPerView: 5,
+        spaceBetween: 80,
+        autoplay: {
+            delay: 0,
+            disableOnInteraction: false,
+        },
+
+        speed: 5000,
+    });
+
+    const main_product_slide = new Swiper('.main_product_slide', {
+        loop: true,
+        slidesPerView: 4,
+        spaceBetween: 30,
+
+        navigation: {
+            nextEl: '.main_product .next',
+            prevEl: '.main_product .prev',
+        },
+    });
+
+    $('').on('click', function () {
+        main_visual_slide.slidePrev();
+    });
+
+    $('').on('click', function () {
+        main_visual_slide.slideNext();
+    });
+
+});
+
+$(function () {
+    $('.to_top').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 })
+    });
+
+    $(window).on('scroll', function () {
+        //스크롤 된 양을 구함.
+        let sct = $(window).scrollTop();
+        console.log(sct)
+
+        if (sct > 400) {
+            $('.to_top').addClass('on')
+        } else {
+            $('.to_top').removeClass('on')
+        }
+    });
+
+})
